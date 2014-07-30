b45f121a172625dacf7f1574e276567a46f7ab99
jlapuca <jlapuca@github.com>
2014-07-30 16:44:12 +0400

    code changes

--- get-all-dids.py
+++ get-all-dids.py
@@ -10,22 +10,23 @@ for line in exts:
         print "Start parsing..."
 
         # разделим строку на отдельные части там где были пробелы
-        fields = line.split(' ')
+        fields = line.split(' ')[2].split(',')
         print fields
 
         # откусываем заднюю часть
-        back = fields[2]
+        #back = fields[2]
 
         # разделяем ее на части по запятым:
-        fields = back.split(',')
-        print fields
+        #fields = back.split(',')
+        #print fields
 
         # первый кусок целиком попал в 0-й элемент
-        first_number = fields[0]
+        number = fields[0]+'\t'+fields[3]
 
         # второй кусок целиком попал в 3-й элемент
-        second_number = fields[3]
+        #second_number = fields[3]
 
-        print "Find numbers:", first_number, second_number
+        #print "Find numbers:", first_number, second_number
+        print "find numbers:", number
 
 exts.close()
