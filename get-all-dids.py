d23af0815318bdd44c37cf8be9ade46f03ca8f7f
sp1r <namelessorama@gmail.com>
2014-07-30 16:46:41 +0400

    в две строки

--- get-all-dids.py
+++ get-all-dids.py
@@ -5,28 +5,7 @@ exts = open("extensions.conf", "r")
 
 for line in exts:
     if "internalq-calls" in line and not line.startswith(';'):
-        # для начала расписываю подробно, потом ужмем
-        print "Got good line:", line,
-        print "Start parsing..."
-
-        # разделим строку на отдельные части там где были пробелы
         fields = line.split(' ')[2].split(',')
-        print fields
-
-        # откусываем заднюю часть
-        #back = fields[2]
-
-        # разделяем ее на части по запятым:
-        #fields = back.split(',')
-        #print fields
-
-        # первый кусок целиком попал в 0-й элемент
-        number = fields[0]+'\t'+fields[3]
-
-        # второй кусок целиком попал в 3-й элемент
-        #second_number = fields[3]
-
-        #print "Find numbers:", first_number, second_number
-        print "find numbers:", number
+        print "find numbers: %s\t%s" % (fields[0], fields[3])
 
 exts.close()
