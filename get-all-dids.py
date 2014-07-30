2ee45bf9b3bc6ec0420e704991a073667c3c8ca1
jlapuca <jlapuca@github.com>
2014-07-30 15:54:12 +0400

    add data file

--- get-all-dids.py
+++ get-all-dids.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 
-exts = open("/home/lyakovleva/extensions.conf", "r")
+exts = open("./extensions.conf", "r")
 for line in exts:
   if "internalq-calls" in line and ';' not in line:
     print "This is good line:", line,
