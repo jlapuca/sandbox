dd1f462dad956b8e386a48aa3142d660a2f21eca
jlapuca <jlapuca@github.com>
2014-07-31 18:36:52 +0400

    find 'names' with configparser

--- get-all-dids.py
+++ get-all-dids.py
@@ -9,11 +9,14 @@ users.read('users.conf')
 
 print "%-20s\t%s\t%20s" % ('Name','ext', 'DID')
 
+def getUsername (section):
+    user = users.get(section, 'fullname')
+    return user
+
 for line in exts:
     if "internalq-calls" in line and not line.startswith(';'):
         fields = line.split(' ')[2].split(',')
-        user = users.get(fields[3], 'fullname')
-        print "%-20s\t%s\t%20s" % (user, fields[3], fields[0])
+        print "%-20s\t%s\t%20s" % (getUsername(fields[3]), fields[3], fields[0])
 
 
 exts.close()
